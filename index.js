4ccbba0ef5b84f40a12db0c083f4ad2b3aec2ad2
IMOH SILAS <imosil34@gmail.com>
2020-05-06 16:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,3 +45,10 @@ function makeInt(n){
 function makeInt(n){
   return parseInt(n,10);
 }
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
+function preserveDecimal(n){
+  return parseFloat("n",10);
+}
