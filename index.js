7db4acf2fb66b9146895c3d249962e35321e44d5
IMOH SILAS <imosil34@gmail.com>
2020-05-06 16:29:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(){
+  return 
+}
+function subtract(){
+  return 
+}
+function multiply(){
+  return 
+}
+function divide(){
+  return 
+}
+function divide(a, b){
+  
+  return (a/b);
+}
+function increment(n){
+  
+  return n++;
+}
+function decrement(n){
+  n--;
+  return n;
+}
